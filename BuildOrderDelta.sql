7b740d17c2068773efdf9f06da80fe6f2a4a638e
Dan Zabinski <dzabinski@peerless-av.com>
2025-09-30 15:39:36 -0500

    Updated Stored Procedure
    Including order delete logic

--- BuildOrderDelta.sql
+++ BuildOrderDelta.sql
@@ -14,7 +14,9 @@ BEGIN
 
         SET @RunID = SCOPE_IDENTITY();
 
-        -- Insert deltas
+        ----------------------------------------------------------------------
+        -- Insert Modified deltas (existing lines that changed)
+        ----------------------------------------------------------------------
         INSERT INTO dbo.OrderDelta (
             SnapshotDate, OrderNbr, LineNbr,
             DeltaOpenQty, DeltaUnitPrice, DeltaExtPrice,
@@ -36,22 +38,73 @@ BEGIN
                      OR cur.OrderStatus <> prev.OrderStatus 
                 THEN 1 ELSE 0 
             END,
-            NULL
+            'Modified'
         FROM dbo.OrderSnapshot cur
         INNER JOIN dbo.OrderSnapshot prev
             ON cur.OrderNbr = prev.OrderNbr
            AND cur.LineNbr = prev.LineNbr
            AND prev.SnapshotDate = @PrevSnapshotDate
-        WHERE cur.SnapshotDate = @SnapshotDate;
+        WHERE cur.SnapshotDate = @SnapshotDate
+          AND (
+                cur.OpenQty    <> prev.OpenQty
+             OR cur.UnitPrice <> prev.UnitPrice
+             OR cur.ExtPrice  <> prev.ExtPrice
+             OR cur.OrderStatus <> prev.OrderStatus
+          );
 
-        SET @RowCount = @@ROWCOUNT;
+        SET @RowCount = @RowCount + @@ROWCOUNT;
 
+        ----------------------------------------------------------------------
+        -- Insert Deleted deltas (prev exists, cur missing)
+        ----------------------------------------------------------------------
+        INSERT INTO dbo.OrderDelta (
+            SnapshotDate, OrderNbr, LineNbr,
+            DeltaOpenQty, DeltaUnitPrice, DeltaExtPrice,
+            DeltaPct, DeltaStatusChange, IsSignificant, Notes
+        )
+        SELECT
+            @SnapshotDate,
+            prev.OrderNbr,
+            prev.LineNbr,
+            -prev.OpenQty,
+            -prev.UnitPrice,
+            -prev.ExtPrice,
+            NULL,
+            0,
+            1,
+            'DeletedLine'
+        FROM dbo.OrderSnapshot prev
+        LEFT JOIN dbo.OrderSnapshot cur
+          ON cur.OrderNbr = prev.OrderNbr
+         AND cur.LineNbr  = prev.LineNbr
+         AND cur.SnapshotDate = @SnapshotDate
+        WHERE prev.SnapshotDate = @PrevSnapshotDate
+          AND cur.OrderNbr IS NULL;
+
+        SET @RowCount = @RowCount + @@ROWCOUNT;
+
+        ----------------------------------------------------------------------
+        -- Update previous snapshot rows to mark as Deleted
+        ----------------------------------------------------------------------
+        UPDATE s
+        SET s.Notes = 'Deleted'
+        FROM dbo.OrderSnapshot s
+        LEFT JOIN dbo.OrderSnapshot cur
+          ON cur.OrderNbr = s.OrderNbr
+         AND cur.LineNbr  = s.LineNbr
+         AND cur.SnapshotDate = @SnapshotDate
+        WHERE s.SnapshotDate = @PrevSnapshotDate
+          AND cur.OrderNbr IS NULL;
+
+        ----------------------------------------------------------------------
         -- Update log success
+        ----------------------------------------------------------------------
         UPDATE dbo.OrderDeltaRunLog
         SET RunEndTime = SYSDATETIME(),
             RowsInserted = @RowCount,
             RunStatus = 'Success'
         WHERE RunID = @RunID;
+
     END TRY
     BEGIN CATCH
         SET @ErrorMessage = ERROR_MESSAGE();
